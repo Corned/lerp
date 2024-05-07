6cda93151c4cb6059de747cd378fe1c5078a79e2
Oliver K <Corned@users.noreply.github.com>
2024-05-07 10:54:37 +0300

    moved from CRA to vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css"
+import { useState } from "react"
 
 function App() {
-  return <div className="App"></div>
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+      >
+        {count}
+      </button>
+    </>
+  )
 }
 
 export default App
