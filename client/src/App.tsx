39ce55a24a80bdb35f41aebd0ee48064715bcc84
Corned <Corned@users.noreply.github.com>
2024-05-10 14:56:26 +0300

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import ProfilePicture from "./components/ProfilePicture"
 import Workspace from "./components/Workspace"
 
-function App() {
+const App = () => {
   return (
     <div className="App grid h-full grid-rows-[60px_1fr]">
       <nav className="flex flex-row items-center justify-between border-b py-2 pl-10 pr-2">
