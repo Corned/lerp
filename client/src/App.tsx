15f35519340f2c737134ee6dafedd3719b37af99
Oliver K <Corned@users.noreply.github.com>
2024-05-13 10:09:19 +0300

    removed unused code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,6 @@ import { ChevronDown, Home } from "lucide-react"
 import ProfilePicture from "./components/ProfilePicture"
 import Workspace from "./components/Workspace"
 
-const MiniChevronDownIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      className="h-5 w-5"
-    >
-      <path
-        fillRule="evenodd"
-        d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-        clipRule="evenodd"
-      />
-    </svg>
-  )
-}
-
 const App = () => {
   return (
     <div className="App grid h-full grid-rows-[70px_1fr]">
