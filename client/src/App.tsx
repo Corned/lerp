e65ae5aa313110c3e8f6adc76e0b32b4f7ffe54d
Oliver K <Corned@users.noreply.github.com>
2024-05-14 09:15:41 +0300

    removed the logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,14 +7,7 @@ const App = () => {
     <div className="App grid h-full grid-rows-[70px_1fr]">
       <nav className="flex flex-row items-center justify-between border-b py-3 pl-10 pr-3">
         <h1 className="text-2xl font-bold">LERP</h1>
-
-        <div className="logo">
-          <div className="a"></div>
-          <div className="b"></div>
-          <div className="c"></div>
-          <div className="d"></div>
-        </div>
-
+        
         <div className="ml-10 mr-auto flex flex-row gap-2">
           <button className="flex flex-row items-center justify-center gap-2 rounded-md px-3 py-2 text-xl font-bold hover:bg-black/10 ">
             <span>Home</span>
