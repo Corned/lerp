fc3850c24ff4cc3d58be03a223c84cc25a6018d0
Oliver K <Corned@users.noreply.github.com>
2024-05-15 10:01:29 +0300

    redux toolkit + rtk query stuff

---
+++ client/src/services/taskApi.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const taskApi = createApi({
+  reducerPath: "pathApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/tasks" }),
+  endpoints: (builder) => ({
+    getTasksByWorkspaceId: builder.query({
+      query: (id) => `?workspace=${id}`,
+    }),
+  }),
+})
+
+export const { useGetTasksByWorkspaceIdQuery } = taskApi
