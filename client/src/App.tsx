8fbe3c1fa740506d3eea4e70c6be9943a7535daf
Oliver K <Corned@users.noreply.github.com>
2024-05-20 14:20:31 +0300

    using shadcn components in nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,34 @@ import { ChevronDown, Home } from "lucide-react"
 import { Outlet } from "react-router-dom"
 
 import ProfilePicture from "@/components/ProfilePicture"
+import { Button } from "./components/ui/button"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "./components/ui/navigation-menu"
 
 const App = () => {
   return (
     <div className="App grid h-full grid-rows-[70px_1fr]">
       <nav className="flex flex-row items-center justify-between border-b py-3 pl-10 pr-3">
-        <h1 className="text-2xl font-bold">LERP</h1>
+        <Button variant="link" className="text-2xl font-bold px-10">
+          LERP
+        </Button>
 
-        <div className="ml-10 mr-auto flex flex-row gap-2">
-          <button className="flex flex-row items-center justify-center gap-2 rounded-md px-3 py-2 text-xl font-bold hover:bg-black/10 ">
-            <span>Home</span>
-            <Home size={18} />
-          </button>
-          <button className="flex flex-row items-center justify-center gap-2 rounded-md px-3 py-2 text-xl font-bold hover:bg-black/10 ">
-            <span>My Workspaces</span>
-            <ChevronDown size={18} />
-          </button>
-        </div>
+        <NavigationMenu className="mr-auto">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="text-xl">My Workspaces</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <div className="p-5 flex flex-col gap-4">
+                  <div className="">
+                    <p className="text">Lerp Development</p>
+                  </div>
+                  <div className="">
+                    <p className="text">Another Workspace</p>
+                  </div>
+                </div>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
 
         <ProfilePicture />
       </nav>
